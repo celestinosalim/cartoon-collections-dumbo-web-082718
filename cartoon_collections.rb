b8425eb42af00785051a7debe7fda91709c2a275
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-22 13:22:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(list)
-  list.each {|name, i| puts "#{i[]}, #{name}" }
-
+  list.each do |name, i| 
+  puts "#{i + 1}, #{name}" 
+  end
 end
 
 def summon_captain_planet# code an argument here
