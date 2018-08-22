06d89eac8ef1447c82a297ba1615b0ac8c1fae33
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-22 13:31:32 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,11 +9,16 @@ def summon_captain_planet(list)
     element = "#{element.capitalize}!"
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? do |call|
+    call.length > 4
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+
+  array.find do |item|
+    if cheese_types.include?(item)
+      item
+    end
 end
