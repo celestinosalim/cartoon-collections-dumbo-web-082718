43f03a5b34df6a213f023871b214c68a33f5e9ab
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-22 13:20:09 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(list)
+  list.each {|name, i| puts "#{i + 1}, #{name}" }
+  
 end
 
 def summon_captain_planet# code an argument here
